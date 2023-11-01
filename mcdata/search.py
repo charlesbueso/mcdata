42731b8c78a50c293d014bf5db2b86f5563940d7
Carlos Adrian Bueso <37254269+charlesbueso@users.noreply.github.com>
2023-10-31 21:18:05 -0500

    add search bar and basic UI (#2)

---
+++ mcdata/search.py
@@ -0,0 +1,6 @@
+def search_database(input):
+
+    search_results = 'This yo mfin search results!!!\n your input:  ' + input
+    #TODO: Add database search, later semantic/filtered search
+
+    return search_results
