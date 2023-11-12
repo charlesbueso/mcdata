96683d7f4a8937e709b23549abb25f12be714b8f
Carlos Bueso <charlesbueso@gmail.com>
2023-11-12 13:28:15 -0600

    first working portal

--- mcdata-frontend/src/datasets/threedanimation.jsx
+++ mcdata-frontend/src/datasets/threedanimation.jsx
@@ -1,5 +1,5 @@
-import { BackSide } from "three";
-import { Environment, OrbitControls, useTexture } from "@react-three/drei";
+import { BackSide, DoubleSide } from "three";
+import { Environment, MeshPortalMaterial, OrbitControls, RoundedBox, useTexture } from "@react-three/drei";
 import { Punk } from "../threecomponents/Punk"
 
 export const Experience = () => {
@@ -11,11 +11,24 @@ export const Experience = () => {
         <OrbitControls />
         <ambientLight intensity={.5} />
         <Environment preset="sunset" />
-        <mesh>
-            <Punk scale={1.5} position-y={-1}/>
-            <sphereGeometry args={[5,64,64]} />
-            <meshStandardMaterial map={map} side={BackSide} />
-        </mesh>
+        <RoundedBox args={[2,3,0.1]}>
+            <MeshPortalMaterial side={DoubleSide}>
+                <ambientLight intensity={1} />
+                <Environment preset="sunset" />
+                <Punk scale={1.2} position-y={-1.3}/>
+                <mesh>
+                <sphereGeometry args={[5,64,64]} />
+                <meshStandardMaterial map={map} side={BackSide} />
+                </mesh>
+            </MeshPortalMaterial>
+        </RoundedBox>
         </>
     )
 }
+
+// const MonsterStage = ({...props}) => {
+//     return (
+//     <group {...props}>
+
+//     </group>)
+// }
